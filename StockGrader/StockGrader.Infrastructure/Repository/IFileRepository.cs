bc0c1ee6680fcbd3e34fca15bb21ef42c9afc43b
Milan Slovak <milan.slovak@thermofisher.com>
2023-03-28 13:29:33 +0200

    #1 Download data from web
    - added file repo for downloading files

---
+++ StockGrader/StockGrader.Infrastructure/Repository/IFileRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace StockGrader.Infrastructure.Repository
+{
+    public interface IFileRepository
+    {
+        /// <summary>
+        /// Download the web resource and save it into the file system folder.
+        /// </summary>
+        /// <param name="address">Web resource</param>
+        /// <param name="location">File system folder location</param>
+        Task Fetch(Uri address, string location);
+    }
+}
