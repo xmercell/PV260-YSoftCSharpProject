7b51f6b08e9339942a352498e89d8a281811c648
Milan Slovak <milan.slovak@thermofisher.com>
2023-03-28 22:16:44 +0200

    #2 Store downloaded data
    - removed regex generators

--- StockGrader/StockGrader.Domain/Model/ReportEntry.cs
+++ StockGrader/StockGrader.Domain/Model/ReportEntry.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace StockGrader.Domain.Model
 {
-    public partial class ReportEntry
+    public class ReportEntry
     {
         [Required]
         public DateTime Date { get; set; }
@@ -33,17 +33,14 @@ namespace StockGrader.Domain.Model
         {
             string[] values = line.Split(',');
 
-            Date = Convert.ToDateTime(Numbers().Replace(values[0], ""));
+            Date = Convert.ToDateTime(Regex.Replace(values[0], "[^0-9]", ""));
             Fund = Convert.ToString(values[1]);
             CompanyName = Convert.ToString(values[2]);
             Ticker = Convert.ToString(values[3]);
             Cusip = Convert.ToString(values[4]);
-            Shares = Convert.ToInt32(Numbers().Replace(values[5], ""));
-            MarketValue = Convert.ToDouble(Numbers().Replace(values[6], ""));
-            Weight = Convert.ToDouble(Numbers().Replace(values[7], ""));
+            Shares = Convert.ToInt32(Regex.Replace(values[5], "[^0-9]", ""));
+            MarketValue = Convert.ToDouble(Regex.Replace(values[6], "[^0-9.]", ""));
+            Weight = Convert.ToDouble(Regex.Replace(values[7], "[^0-9.]", ""));
         }
-
-        [GeneratedRegex("[^0-9]")]
-        private static partial Regex Numbers();
     }
 }
