e13bd587977de9b15b6563942c0951e8ef360728
Tamara Babálová <xbabalov@fi.muni.cz>
2023-03-28 21:52:38 +0200

    fix entry parsing

--- StockGrader/StockGrader.Domain/Model/ReportEntry.cs
+++ StockGrader/StockGrader.Domain/Model/ReportEntry.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace StockGrader.Domain.Model
 {
@@ -33,14 +34,14 @@ namespace StockGrader.Domain.Model
             string[] values = line.Split(',');
             ReportEntry entry = new()
             {
-                Date = Convert.ToDateTime(values[0]),
+                Date = Convert.ToDateTime(Regex.Replace(values[0], "[^0-9]", "-")),
                 Fund = Convert.ToString(values[1]),
                 CompanyName = Convert.ToString(values[2]),
                 Ticker = Convert.ToString(values[3]),
                 Cusip = Convert.ToString(values[4]),
-                Shares = Convert.ToInt32(values[5]),
-                MarketValue = Convert.ToDouble(values[6]),
-                Weight = Convert.ToDouble(values[7])
+                Shares = Convert.ToInt32(Regex.Replace(values[5], "[^0-9]", "")),
+                MarketValue = Convert.ToDouble(Regex.Replace(values[6], "[^0-9.]", "")),
+                Weight = Convert.ToDouble(Regex.Replace(values[7], "[^0-9.]", ""))
             };
 
             return entry;
