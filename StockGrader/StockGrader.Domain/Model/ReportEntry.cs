dad21e94e3fc279a2838e89e4ec6e2e4cc5d8392
Tamara Babálová <xbabalov@fi.muni.cz>
2023-03-28 21:33:38 +0200

    Report and entry parsers added

--- StockGrader/StockGrader.Domain/Model/ReportEntry.cs
+++ StockGrader/StockGrader.Domain/Model/ReportEntry.cs
@@ -5,7 +5,7 @@ namespace StockGrader.Domain.Model
     public class ReportEntry
     {
         [Required]
-        public DateTime EntryDate { get; set; }
+        public DateTime Date { get; set; }
 
         public string Fund { get; set; }
 
@@ -26,6 +26,24 @@ namespace StockGrader.Domain.Model
         public double MarketValue { get; set; }
 
         [Required]
-        public float Weight { get; set; }
+        public double Weight { get; set; }
+
+        public static ReportEntry FromCSV(string line)
+        {
+            string[] values = line.Split(',');
+            ReportEntry entry = new()
+            {
+                Date = Convert.ToDateTime(values[0]),
+                Fund = Convert.ToString(values[1]),
+                CompanyName = Convert.ToString(values[2]),
+                Ticker = Convert.ToString(values[3]),
+                Cusip = Convert.ToString(values[4]),
+                Shares = Convert.ToInt32(values[5]),
+                MarketValue = Convert.ToDouble(values[6]),
+                Weight = Convert.ToDouble(values[7])
+            };
+
+            return entry;
+        }
     }
 }
--- StockGrader/StockGrader.Domain/Model/StockReport.cs
+++ StockGrader/StockGrader.Domain/Model/StockReport.cs
@@ -2,6 +2,27 @@
 {
     public class StockReport
     {
-        public IEnumerable<ReportEntry> Entries { get; set; }
+        public IEnumerable<ReportEntry> Entries { get; set; } = new List<ReportEntry>();
+
+        public static StockReport FromCSV(string path)
+        {
+            StockReport report = new StockReport
+            {
+                Entries = new List<ReportEntry>()
+            };
+
+            using (var reader = new StreamReader(@path))
+            {
+                reader.ReadLine(); //header
+
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    report.Entries.Append(ReportEntry.FromCSV(line));
+                }
+            }
+
+            return report;
+        }
     }
 }
