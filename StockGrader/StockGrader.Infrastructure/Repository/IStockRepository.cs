fa6985a30c057137931f749ac8e7228682482116
Kristian Mika <kristian.mika@protonmail.com>
2023-03-29 22:34:21 +0200

    set up StockRepo in the constructor

--- StockGrader/StockGrader.Infrastructure/Repository/IStockRepository.cs
+++ StockGrader/StockGrader.Infrastructure/Repository/IStockRepository.cs
@@ -7,10 +7,8 @@ namespace StockGrader.Infrastructure.Repository
         /// <summary>
         /// Fetch fresh stock report from csv file on the web address: 
         /// </summary>
-        /// <param name="holdingsSheetUri">URI of the file to download</param>
-        /// <param name="reportFilepath">filepath to be used for the new report</param>
         /// <returns>New stock report parsed from csv</returns>
-        Task<StockReport> FetchNew(Uri holdingsSheetUri, string reportFilepath);
+        Task<StockReport> FetchNew();
 
     }
 }
--- StockGrader/StockGrader.Infrastructure/Repository/StockRepository.cs
+++ StockGrader/StockGrader.Infrastructure/Repository/StockRepository.cs
@@ -5,16 +5,20 @@
         public class StockRepository : IStockRepository
         {
             private readonly IFileRepository _fileRepository;
+            private readonly Uri _holdingsSheetUri;
+            private readonly string _reportFilepath;
 
-            public StockRepository(IFileRepository fileRepository)
+            public StockRepository(IFileRepository fileRepository, Uri holdingsSheetUri, string reportFilepath)
             {
                 _fileRepository = fileRepository;
+                _holdingsSheetUri = holdingsSheetUri;
+                _reportFilepath = reportFilepath;
             }
             
-            public async Task<StockReport> FetchNew(Uri holdingsSheetUri, string reportFilepath)
+            public async Task<StockReport> FetchNew()
             {
-                await _fileRepository.Fetch(holdingsSheetUri, reportFilepath);
-                var report = new StockReport(reportFilepath);
+                await _fileRepository.Fetch(_holdingsSheetUri, _reportFilepath);
+                var report = new StockReport(_reportFilepath);
                 return report;
             }
         }
