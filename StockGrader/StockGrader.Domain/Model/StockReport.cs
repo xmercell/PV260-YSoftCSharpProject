a4ec9ed43173e29a9d10e6e18d70a2305eb3c096
Milan Slovak <milan.slovak@thermofisher.com>
2023-04-03 01:16:34 +0200

    Refactored stock repository

--- StockGrader/StockGrader.Domain/Model/StockReport.cs
+++ StockGrader/StockGrader.Domain/Model/StockReport.cs
@@ -1,35 +1,7 @@
-﻿using CsvHelper;
-using CsvHelper.Configuration;
-using System.Globalization;
-
-namespace StockGrader.Domain.Model
+﻿namespace StockGrader.Domain.Model
 {
     public class StockReport
     {
         public IEnumerable<ReportEntry> Entries { get; set; } = new List<ReportEntry>();
-
-        public StockReport(string filePath)
-        {
-            using var reader = new StreamReader(filePath);
-            using var csv = new CsvReader(reader, GetConfig());
-            csv.Context.RegisterClassMap(new ReportEntryMap());
-            Entries = csv.GetRecords<ReportEntry>().ToList();
-        }
-
-        private CsvConfiguration GetConfig()
-        {
-            return new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                MissingFieldFound = null,
-                ShouldSkipRecord = (row) =>
-                {
-                    if (row.Row.HeaderRecord != null)
-                    {
-                        return !DateTime.TryParse(row.Row.GetField("date"), out _);
-                    }
-                    return false;
-                }
-            };
-        }
     }
 }
--- StockGrader/StockGrader.Infrastructure/Repository/StockRepository.cs
+++ StockGrader/StockGrader.Infrastructure/Repository/StockRepository.cs
@@ -1,28 +1,53 @@
-﻿    using StockGrader.Domain.Model;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using StockGrader.Domain.Model;
+using System.Globalization;
 
-    namespace StockGrader.Infrastructure.Repository
+namespace StockGrader.Infrastructure.Repository
+{
+    public class StockRepository : IStockRepository
     {
-        public class StockRepository : IStockRepository
+        private readonly IFileRepository _fileRepository;
+        private readonly Uri _holdingsSheetUri;
+        private readonly string _reportFilePath;
+
+        public StockRepository(IFileRepository fileRepository, Uri holdingsSheetUri, string reportFilePath)
         {
-            private readonly IFileRepository _fileRepository;
-            private readonly Uri _holdingsSheetUri;
-            private readonly string _reportFilePath;
+            _fileRepository = fileRepository;
+            _holdingsSheetUri = holdingsSheetUri;
+            _reportFilePath = reportFilePath;
+        }
 
-            public StockRepository(IFileRepository fileRepository, Uri holdingsSheetUri, string reportFilePath)
-            {
-                _fileRepository = fileRepository;
-                _holdingsSheetUri = holdingsSheetUri;
-                _reportFilePath = reportFilePath;
-            }
+        public async Task FetchNew()
+        {
+            await _fileRepository.Fetch(_holdingsSheetUri, _reportFilePath);
+        }
 
-            public async Task FetchNew()
-            {
-                await _fileRepository.Fetch(_holdingsSheetUri, _reportFilePath);
-            }
+        public StockReport GetLast()
+        {
+            using var reader = new StreamReader(_reportFilePath);
 
-            public StockReport GetLast()
+            using var csv = new CsvReader(reader, GetConfig());
+            csv.Context.RegisterClassMap(new ReportEntryMap());
+            
+            var rows = csv.GetRecords<ReportEntry>().ToList();
+            return new StockReport { Entries = rows };
+        }
+
+        private CsvConfiguration GetConfig()
+        {
+            return new CsvConfiguration(CultureInfo.InvariantCulture)
             {
-                return new StockReport(_reportFilePath);
-            }
+                MissingFieldFound = null,
+                ShouldSkipRecord = (row) =>
+                {
+                    if (row.Row.HeaderRecord != null)
+                    {
+                        return !DateTime.TryParse(row.Row.GetField("date"), out _);
+                    }
+                    return false;
+                }
+            };
         }
     }
+}
