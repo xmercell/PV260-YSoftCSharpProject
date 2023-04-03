ca88ef4ebf18580132b13fa0a191ff2ab39cdac0
Milan Slovak <milan.slovak@thermofisher.com>
2023-04-03 22:24:33 +0200

    Fixed comma/dot conflict

--- StockGrader/StockGrader.Evaluator.Test/PositionsTests.cs
+++ StockGrader/StockGrader.Evaluator.Test/PositionsTests.cs
@@ -9,25 +9,28 @@ namespace StockGrader.Evaluator.Test
         public void TestPositionToString()
         {
             var position = new Position("Microsoft", "MSFT", 100, 0.5);
-            Assert.AreEqual("Microsoft, MSFT, 100, 0.5", position.ToString());
+            Assert.That(position.ToString(), Is.EqualTo($"Microsoft, MSFT, 100, {0.5}"));
         }
+
         [Test]
         public void TestRemovedPositionToString()
         {
             var removedPosition = new RemovedPosition("Microsoft", "MSFT");
             Assert.That(removedPosition.ToString(), Is.EqualTo("Microsoft, MSFT"));
         }
+
         [Test]
         public void TestIncreasedPositionToString()
         {
             var updatedPosition = new UpdatedPosition("Microsoft", "MSFT", 100, 0.5, 10);
-            Assert.That(updatedPosition.ToString(), Is.EqualTo("Microsoft, MSFT, 100(🔺10%), 0.5"));
+            Assert.That(updatedPosition.ToString(), Is.EqualTo($"Microsoft, MSFT, 100(🔺10%), {0.5}"));
         }
+
         [Test]
         public void TestDecreasedPositionToString()
         {
             var updatedPosition = new UpdatedPosition("Microsoft", "MSFT", 100, 0.5, -10);
-            Assert.That(updatedPosition.ToString(), Is.EqualTo("Microsoft, MSFT, 100(🔻10%), 0.5"));
+            Assert.That(updatedPosition.ToString(), Is.EqualTo($"Microsoft, MSFT, 100(🔻10%), {0.5}"));
         }
     }
 }
