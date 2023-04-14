79a62ee1dc45c1b1d89fefdc7b868517c4f87684
Milan Slovak <milan.slovak@thermofisher.com>
2023-04-14 15:02:56 +0200

    #18 Dependency inversion
    - Added installer for DAL

---
+++ StockGrader/StockGrader.DAL/Installer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StockGrader.DAL.Repository;
+using System.Runtime.CompilerServices;
+
+namespace StockGrader.DAL
+{
+    public static class Installer
+    {
+        public static void InstallDal(this IServiceCollection collection)
+        {
+            collection.AddTransient<IFileRepository, FileRepository>();
+            collection.AddTransient<IStockRepository, StockRepository>();
+        }
+    }
+}
