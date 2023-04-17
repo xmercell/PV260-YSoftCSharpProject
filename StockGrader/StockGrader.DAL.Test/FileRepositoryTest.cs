c29a99ba4c584d81c10f15e41492974867d4c2e6
xbabalov <t.babalova.17@gmail.com>
2023-04-17 16:55:21 +0200

    DAL.Test project and FileRepository Test added

---
+++ StockGrader/StockGrader.DAL.Test/FileRepositoryTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
+using StockGrader.DAL.Repository;
+
+namespace StockGrader.DAL.Test
+{
+    public class FileRepositoryTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task FetchTest()
+        {
+            var fileRep = new FileRepository();
+            var fileName = Path.GetTempFileName();
+
+            try
+            {
+                await fileRep.Fetch(new Uri("https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv"), fileName);
+                using StreamReader sr = new StreamReader(File.OpenRead(fileName));
+                var header = await sr.ReadLineAsync();
+                Assert.That(header, Is.Not.Null);
+                Assert.That(header.Equals("date,fund,company,ticker,cusip,shares,\"market value ($)\",\"weight (%)\""));
+            }
+            finally
+            {
+                File.Delete(fileName);
+            }
+        }
+    }
+}
