2d77286a4c74630c03bc3322be4e1a3f55357ffc
Milan Slovak <milan.slovak@thermofisher.com>
2023-04-25 13:43:39 +0200

    #40 Rework notes from seminar review
    - added diff model

---
+++ StockGrader/StockGrader.BL/Model/Diff.cs
@@ -0,0 +1,11 @@
+﻿namespace StockGrader.BL.Model
+{
+    public class Diff
+    {
+        public IList<Position> NewPositions { get; } = new List<Position>();
+        public IList<Position> UnchangedPositions { get; } = new List<Position>();
+        public IList<UpdatedPosition> IncreasedPositions { get; } = new List<UpdatedPosition>();
+        public IList<UpdatedPosition> ReducedPositions { get; } = new List<UpdatedPosition>();
+        public IList<RemovedPosition> RemovedPositions { get; } = new List<RemovedPosition>();
+    }
+}
