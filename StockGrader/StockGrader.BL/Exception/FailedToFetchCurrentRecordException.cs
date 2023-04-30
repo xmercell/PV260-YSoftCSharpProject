5aa07415cb29fac90d76c66ed9a4a940fcb95759
davedr <david.184@seznam.cz>
2023-04-30 16:50:42 +0200

    feat: added BL exceptions
    Signed-off-by: davedr <david.184@seznam.cz>

---
+++ StockGrader/StockGrader.BL/Exception/FailedToFetchCurrentRecordException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockGrader.BL.Exception
+{
+    public class FailedToFetchCurrentRecordException : System.Exception
+    {
+        public FailedToFetchCurrentRecordException()
+        {
+        }
+
+        public FailedToFetchCurrentRecordException(string message)
+            : base(message)
+        {
+        }
+
+        public FailedToFetchCurrentRecordException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
---
+++ StockGrader/StockGrader.BL/Exception/FailedToGetOldRecordException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockGrader.BL.Exception
+{
+    public class FailedToGetOldRecordException : System.Exception
+    {
+        public FailedToGetOldRecordException()
+        {
+        }
+
+        public FailedToGetOldRecordException(string message)
+            : base(message)
+        {
+        }
+
+        public FailedToGetOldRecordException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
