f2bda605c2ec6c0cc43098a017b7478db445fa0a
xbabalov <t.babalova.17@gmail.com>
2023-05-01 14:41:21 +0200

    DAL tests changed to required format

--- StockGrader/StockGrader.DAL.Test/StockDiscRepositoryTest.cs
+++ StockGrader/StockGrader.DAL.Test/StockDiscRepositoryTest.cs
@@ -10,49 +10,22 @@ namespace StockGrader.DAL.Test
 {
     public class StockDiscRepositoryTest
     {
-
-
-        private StockDiscRepository stockRep;
-
-        [SetUp]
-        public void SetUp()
-        {
-            var endpointUri = "https://matejgros.documents.azure.com:443/";
-            var primaryKey = Environment.GetEnvironmentVariable("PRIMARY_KEY");
-            var databaseName = "ysoft";
-            var containerName = "ysoft_con";
-
-            var url = new Uri("https://ark-funds.com/wp-content/uploads/funds-etf-csv/ARK_INNOVATION_ETF_ARKK_HOLDINGS.csv");
-            var userAgentHeader = "User-Agent";
-            var commonUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
-            stockRep = new StockDiscRepository(url, userAgentHeader, commonUserAgent, endpointUri, primaryKey, databaseName, containerName);
-        }
-
         [Test]
-        public async Task StockDiskRepositoryLoadsCsv()
+        public void StockDiskRepositoryLoadsCsv()
         {
-            await stockRep.FetchNew();
-            var stockReport = stockRep.GetLast();
-            Assert.That(stockReport, Is.Not.Null);
-            Assert.That(stockReport.Entries, Is.Not.Null);
-            Assert.That(stockReport.Entries.Count(), Is.GreaterThan(0));
+            new When().LoadNewReportAsync().Result.AssertReportNotEmpty();
         }
 
         [Test]
         public void GetByRealDateTest()
         {
-            var stockReport = stockRep.GetByDate(new DateTime(2023, 4, 28, 1, 1, 1, DateTimeKind.Utc));
-            Assert.That(stockReport, Is.Not.Null);
-            Assert.That(stockReport.Entries, Is.Not.Null);
-            Assert.That(stockReport.Entries.Count(), Is.EqualTo(28));
+            new When().LoadReportValidDate().AssertReportEntriesCount();
         }
 
         [Test]
         public void GetByNonExistDateTest()
         {
-            var stockReport = stockRep.GetByDate(new DateTime(2023, 3, 28, 1, 1, 1, DateTimeKind.Utc));
-            Assert.That(stockReport, Is.Null);
-
+            new When().LoadReportInvalidDate().AssertReportEmpty();
         }
 
 
